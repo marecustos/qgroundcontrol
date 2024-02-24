6358ca8db6ff8687d993fddb569d029c70ceb0d1
offgrid88 <45432048+offgrid88@users.noreply.github.com>
2024-02-24 14:26:20 +0100

    draft wf test
    Signed-off-by: offgrid88 <45432048+offgrid88@users.noreply.github.com>

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -3,5 +3,4 @@
 
 docker build --file ./deploy/docker/Dockerfile-build-linux -t qgc-linux-docker .
 mkdir  "$(pwd)"/build
- chown -R $USER:$USER "$(pwd)"/build
 docker run --rm -v ${PWD}:/project/source -v ${PWD}/build:/project/build qgc-linux-docker
