322f5f02f70fbab68f9548d903924e349fc3f042
offgrid88 <45432048+offgrid88@users.noreply.github.com>
2024-04-03 15:18:22 +0100

    include the qt in the package

--- deb_build.sh
+++ deb_build.sh
@@ -4,6 +4,13 @@
 . deploy/docker/build-release.sh
 #remove old compiled qgc
 
+#if you want to include the qt in the package incomment this 
+. ./deploy/docker/install-qt-linux.sh
+rm -r deploy/seabot_qgc/opt/Qt
+mkdir -p deploy/seabot_qgc/opt/
+sudo cp -r /opt/Qt deploy/seabot_qgc/
+sudo chown -R $USER:$USER deploy/seabot_qgc/Qt
+
 rm -r deploy/seabot_qgc/usr/bin
 mkdir -p deploy/seabot_qgc/usr/bin
 cp -r seabot_qgc/QGroundControl deploy/seabot_qgc/usr/bin
