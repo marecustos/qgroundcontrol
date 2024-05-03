433093862e80a3f17d464b11cd71e8d6acfa3859
offgrid88 <45432048+offgrid88@users.noreply.github.com>
2024-05-03 16:01:22 +0100

    retest
    Signed-off-by: offgrid88 <45432048+offgrid88@users.noreply.github.com>

--- deb_build.sh
+++ deb_build.sh
@@ -12,23 +12,24 @@ SEABOT_QGC_DIR="deploy/seabot_qgc"
 # rm -r deploy/seabot_qgc/opt/Qt
 
 # Copy Qt to the package
-sudo cp -r "$QT_INSTALL_DIR" "$SEABOT_QGC_DIR/opt/"
-sudo chown -R $USER:$USER "$SEABOT_QGC_DIR/opt/Qt"
+#sudo cp -r "$QT_INSTALL_DIR" "$SEABOT_QGC_DIR/opt/"
+#sudo chown -R $USER:$USER "$SEABOT_QGC_DIR/opt/Qt"
 
 # Copy QGroundControl binary
-rm -r "$SEABOT_QGC_DIR/usr/bin"
-mkdir -p "$SEABOT_QGC_DIR/usr/bin"
-cp -r seabot_qgc/staging/QGroundControl "$SEABOT_QGC_DIR/usr/bin"
+pwd
+rm -r ./"$SEABOT_QGC_DIR/usr/bin"
+mkdir -p ./"$SEABOT_QGC_DIR/usr/bin"
+cp -r ./seabot_qgc/staging/QGroundControl "$SEABOT_QGC_DIR/usr/bin"
 
 # Copy resources
-rm -r "$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
-mkdir -p "$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
-cp -r resources/ "$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
+#rm -r ./"$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
+#mkdir -p ./"$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
+#cp -r resources/ "$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
 
 # Copy icon
-rm -r "$SEABOT_QGC_DIR/usr/share/pixmaps"
-mkdir -p "$SEABOT_QGC_DIR/usr/share/pixmaps"
-cp -r resources/icons/qgroundcontrol.png "$SEABOT_QGC_DIR/usr/share/pixmaps"
+#rm -r "$SEABOT_QGC_DIR/usr/share/pixmaps"
+#mkdir -p "$SEABOT_QGC_DIR/usr/share/pixmaps"
+#cp -r resources/icons/qgroundcontrol.png "$SEABOT_QGC_DIR/usr/share/pixmaps"
 
 # Copy libraries
 #mkdir -p "$SEABOT_QGC_DIR/lib/x86_64-linux-gnu/"
@@ -36,6 +37,8 @@ cp -r resources/icons/qgroundcontrol.png "$SEABOT_QGC_DIR/usr/share/pixmaps"
 #cp seabot_qgc/libs/qmlglsink/libqmlglsink.*so "$SEABOT_QGC_DIR/lib/x86_64-linux-gnu/"
 
 # Build Debian package
-cd deploy
+pwd
+cd ./deploy
 dpkg-deb --build seabot_qgc
 cd ..
+pwd
