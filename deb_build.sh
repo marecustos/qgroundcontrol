7316d96f3967b9fe75fe567bb017c52071030c2a
offgrid88 <45432048+offgrid88@users.noreply.github.com>
2024-05-03 18:49:09 +0100

    retest
    Signed-off-by: offgrid88 <45432048+offgrid88@users.noreply.github.com>

--- deb_build.sh
+++ deb_build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Set up variables
-SEABOT_QGC_DIR="${{ github.workspace }}/deploy/seabot_qgc"
+
 
 # Copy QGroundControl binary
 pwd
-rm -r "$SEABOT_QGC_DIR/usr/bin"
-mkdir -p "$SEABOT_QGC_DIR/usr/bin"
-cp -r "${{ github.workspace }}/seabot_qgc/staging/QGroundControl" "$SEABOT_QGC_DIR/usr/bin"
+rm -r "${{ github.workspace }}/deploy/seabot_qgc/usr/bin"
+mkdir -p "${{ github.workspace }}/deploy/seabot_qgc/usr/bin"
+cp -r "${{ github.workspace }}/seabot_qgc/staging/QGroundControl" "${{ github.workspace }}/deploy/seabot_qgc/usr/bin"
 
 # Copy resources
-rm -r "$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
-mkdir -p "$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
-cp -r "${{ github.workspace }}/resources/" "$SEABOT_QGC_DIR/usr/share/qgroundcontrol"
+rm -r "${{ github.workspace }}/deploy/seabot_qgc/usr/share/qgroundcontrol"
+mkdir -p "${{ github.workspace }}/deploy/seabot_qgc/usr/share/qgroundcontrol"
+cp -r "${{ github.workspace }}/resources/" "${{ github.workspace }}/deploy/seabot_qgc/usr/share/qgroundcontrol"
 
 # Copy icon
-rm -r "$SEABOT_QGC_DIR/usr/share/pixmaps"
-mkdir -p "$SEABOT_QGC_DIR/usr/share/pixmaps"
-cp -r "${{ github.workspace }}/resources/icons/qgroundcontrol.png" "$SEABOT_QGC_DIR/usr/share/pixmaps"
+rm -r "${{ github.workspace }}/deploy/seabot_qgc/usr/share/pixmaps"
+mkdir -p "${{ github.workspace }}/deploy/seabot_qgc/usr/share/pixmaps"
+cp -r "${{ github.workspace }}/resources/icons/qgroundcontrol.png" "${{ github.workspace }}/deploy/seabot_qgc/usr/share/pixmaps"
 
 # Build Debian package
 pwd
